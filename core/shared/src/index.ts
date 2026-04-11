119915768a68ae5059ec66dee61a1274e1a25c22
formula1 <samtobia@gmail.com>
2026-04-11 12:30:03 -0700

    Shred Dep Fix: exporting tree

--- core/shared/src/index.ts
+++ core/shared/src/index.ts
@@ -4,9 +4,10 @@ export * from "./match-lock-file";
 
 export * from "./utils/array";
 export * from "./utils/MessageBridge";
-export * from "./utils/JSON";
-export * from "./utils/string";
-export * from "./utils/fetch";
-export * from "./utils/SimpleEvent";
-export * from "./utils/SimpleMessenger";
 export * from "./utils/Promise";
+export * from "./utils/SimpleMessenger";
+export * from "./utils/tree";
+export * from "./utils/fetch";
+export * from "./utils/JSON";
+export * from "./utils/SimpleEvent";
+export * from "./utils/string";
