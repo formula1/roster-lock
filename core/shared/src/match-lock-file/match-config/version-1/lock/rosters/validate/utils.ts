1b339c988679357d2e40d18fa0d3bdd90bec18f6
formula1 <samtobia@gmail.com>
2026-04-25 21:09:51 -0700

    Cast Fix: localhost test should be "http:" not "http"

--- core/shared/src/match-lock-file/match-config/version-1/lock/rosters/validate/utils.ts
+++ core/shared/src/match-lock-file/match-config/version-1/lock/rosters/validate/utils.ts
@@ -8,6 +8,6 @@ export function validateSha256(value: string){
 export function validateURL(value: string){
   if(!URL.canParse(value)) throw new Error(`${value} is not a valid URL`);
   const url = new URL(value);
-  if(url.protocol === "http" && url.hostname === "localhost") return;
+  if(url.protocol === "http:" && url.hostname === "localhost") return;
   if(!["https:"].includes(url.protocol)) throw new Error(`${value} is not a valid https`);
 }
