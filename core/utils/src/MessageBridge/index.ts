72ed681fec7d00adfb97bb0ce6eff11d99441398
formula1 <samtobia@gmail.com>
2026-07-13 22:07:19 -0700

    Util Feature: bridge can be destroyed

--- core/utils/src/MessageBridge/index.ts
+++ core/utils/src/MessageBridge/index.ts
@@ -10,6 +10,7 @@ import { RequestHandler } from "./request";
 import { SimpleStream, StreamHandler, StreamRequest } from "./stream";
 
 export class MessageBridge {
+  private _destroyed = false;
   private eventHandler: EventHandler
   private requestHandler: RequestHandler;
   private streamHandler: StreamHandler
@@ -29,28 +30,51 @@ export class MessageBridge {
     if(this.requestHandler.handleMessage(message)) return;
     if(this.streamHandler.handleMessage(message)) return;
   }
+  destroy(){
+    if(this._destroyed) return;
+    this._destroyed = true;
+    const error = new Error('Bridge destroyed');
+    for(const pending of Object.values(this.requestHandler.pending)){
+      pending.reject(error);
+    }
+    this.requestHandler.pending = {};
+    for(const pending of Object.values(this.streamHandler.pending)){
+      pending.reject(error);
+    }
+    this.streamHandler.pending = {};
+    for(const stream of Object.values(this.streamHandler.active)){
+      stream.onEnd.emit();
+    }
+    this.streamHandler.active = {};
+  }
 
   sendStream(path: string): StreamRequest {
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.streamHandler.sendStream(path)
   }
 
   onStream(path: string, handler: (stream: SimpleStream)=>any){
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.streamHandler.onStream(path, handler)
   }
 
   sendRequest(path: string, body: any): Promise<any>{
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.requestHandler.sendRequest(path, body);
   }
 
   onRequest(path: string, handler: (data: any) => any){
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.requestHandler.onRequest(path, handler);
   }
 
   sendEvent(path: string, body: any){
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.eventHandler.sendEvent(path, body);
   }
 
   onEvent(path: string, handler: (data: any) => any){
+    if(this._destroyed) throw new Error('Bridge destroyed');
     return this.eventHandler.onEvent(path, handler);
   }
 }
