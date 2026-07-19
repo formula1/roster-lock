dd15153506f9f150db59302c8f65401b43edefaa
formula1 <samtobia@gmail.com>
2026-07-19 15:07:12 -0700

    Client Feature: function to test auth code

--- client/typescript/src/index.ts
+++ client/typescript/src/index.ts
@@ -1,3 +1,4 @@
+import { ROSTERLOCK_MATCH_AGENT_URL } from "./constants/match-agent";
 
 export * from "@roster-lock/types";
 
@@ -8,3 +9,20 @@ export * from "./v1/sync-dl.http";
 export * from "./v1/get-users";
 export * from "./v1/file-routes";
 export * from "./utils/keys";
+
+export async function validateAuthCode(
+  matchAgentAuth: string,
+  matchAgentUrl: string | URL = ROSTERLOCK_MATCH_AGENT_URL,
+){
+  const url = new URL("/validate-authcode", matchAgentUrl);
+  if(!["http:", "https:"].includes(url.protocol)){
+    throw new Error("Expecting The match agent url to be http or https");
+  }
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ authCode: matchAgentAuth })
+  });
+
+  return response.json();
+}
